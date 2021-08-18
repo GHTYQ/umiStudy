e72b8116a653f6ef4b0833b6393397adc55fd69f
yanqingtian <913498714@qq.com>
2021-08-18 10:43:28 +0800

    feature: 添加部署到gh-pages配置

--- .umirc.ts
+++ .umirc.ts
@@ -6,7 +6,15 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  //快速刷新
-  fastRefresh: {},
+  fastRefresh: {}, //快速刷新
+  /**
+   * 路由配置
+   */
   routes: routes,
+  /**
+   * 部署到gh-page配置
+   */
+  outputPath: 'build', //umi打包路径修改，因为gh-page默认是发布build文件夹下内容，而umi默认的打包目录是dist
+  publicPath: 'https://ghtyq.github.io/',
+  history: { type: 'hash' },
 });
